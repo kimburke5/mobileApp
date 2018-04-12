9cea9a53ea2ebd95c10579ea4d1270c7c0f0c64e
kimburke55 <kimburke55@gmail.com>
2018-04-12 15:38:43 +0100

    Buttons added methods generated

--- projectApp/projectApp/MainPage.xaml.cs
+++ projectApp/projectApp/MainPage.xaml.cs
@@ -26,5 +26,100 @@ namespace projectApp
         {
             this.InitializeComponent();
         }
+               
+        //button methods generated
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+           
+            
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonMultiply_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonPercent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void ButtonEquals_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonDecimal_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
     }
 }

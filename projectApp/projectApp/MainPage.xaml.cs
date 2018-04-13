36042531b41453b8ad9662ea3c44a8abe5d5d21a
kimburke55 <kimburke55@gmail.com>
2018-04-13 21:39:54 +0100

    functionality added, app page formatted, images changed

--- projectApp/projectApp/MainPage.xaml.cs
+++ projectApp/projectApp/MainPage.xaml.cs
@@ -18,7 +18,8 @@ using Windows.UI.Xaml.Navigation;
 namespace projectApp
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// A simple calculator app with the ability to add,
+    /// subtract, multiple and divide numbers 
     /// </summary>
     public sealed partial class MainPage : Page
     {
@@ -27,102 +28,164 @@ namespace projectApp
             this.InitializeComponent();
         }
         //Declare and initialize variables
-        private double calc1 = 0, calc2 = 0;
+        private double calc1 = 0.0, calc2 = 0.0;
         bool plusButtonClick = false, minusButtonClick = false, multiplyButtonClick = false, divideButtonClick = false;
 
         //button methods generated
-        //adding functionality to methods
+        //button number 1
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
+
             calcBox.Text = calcBox.Text + "1";
         }
-
+        //button number 2
         private void Button2_Click(object sender, RoutedEventArgs e)
-        {
+        { 
+           
             calcBox.Text = calcBox.Text + "2";
         }
-
+        //button number 3
         private void Button3_Click(object sender, RoutedEventArgs e)
         {
+           
+
             calcBox.Text = calcBox.Text + "3";
         }
-
+        //button number 4
         private void Button4_Click(object sender, RoutedEventArgs e)
         {
+            
+
             calcBox.Text = calcBox.Text + "4";
         }
-
+        //button number 5
         private void Button5_Click(object sender, RoutedEventArgs e)
         {
+           
+
             calcBox.Text = calcBox.Text + "5";
         }
-
+        //button number 6
         private void Button6_Click(object sender, RoutedEventArgs e)
         {
+           
             calcBox.Text = calcBox.Text + "6";
         }
-
+        //button for number 7
         private void Button7_Click(object sender, RoutedEventArgs e)
         {
+            
             calcBox.Text = calcBox.Text + "7";
         }
-
+        //button for number 8
         private void Button8_Click(object sender, RoutedEventArgs e)
         {
-            calcBox.Text = calcBox.Text + "8";
+             calcBox.Text = calcBox.Text + "8";
         }
-
+        //button for number 9
         private void Button9_Click(object sender, RoutedEventArgs e)
         {
             calcBox.Text = calcBox.Text + "9";
         }
-
+        //button for number 0
         private void Button0_Click(object sender, RoutedEventArgs e)
         {
+           
             calcBox.Text = calcBox.Text + "0";
         }
-
+        // + button
         private void ButtonPlus_Click(object sender, RoutedEventArgs e)
         {
-            
-        }
+           
+            calc1 = calc1 += double.Parse(calcBox.Text);
+            calcBox.Text = "";
 
+            //resetting variable results
+            plusButtonClick = true;
+            minusButtonClick = false;
+            multiplyButtonClick = false;
+            divideButtonClick = false;
+        }
+        // - button
         private void ButtonMinus_Click(object sender, RoutedEventArgs e)
         {
-            
-        }
+            calc1 = calc1 + double.Parse(calcBox.Text);
+            calcBox.Text = "";
 
+            plusButtonClick = false;
+            minusButtonClick = true;
+            multiplyButtonClick = false;
+            divideButtonClick = false;
+        }
+        // x button
         private void ButtonMultiply_Click(object sender, RoutedEventArgs e)
         {
-           
-        }
+            calc1 = calc1 + double.Parse(calcBox.Text);
+            calcBox.Text = "";
 
-        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
-        {
+            plusButtonClick = false;
+            minusButtonClick = false;
+            multiplyButtonClick = true;
+            divideButtonClick = false;
             
         }
-
-        private void ButtonPercent_Click(object sender, RoutedEventArgs e)
+        // / button
+        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
         {
+           
+            calc1 = calc1 + double.Parse(calcBox.Text);
+            calcBox.Text = "";
 
+            plusButtonClick = false;
+            minusButtonClick = false;
+            multiplyButtonClick = false;
+            divideButtonClick = true;
         }
 
+        //calculations of the operands
         private void ButtonEquals_Click(object sender, RoutedEventArgs e)
         {
             
-            
+            if (plusButtonClick == true)
+            {
+                calc2 = calc1 + double.Parse(calcBox.Text);
+                calcBox.Text = calc2.ToString();
+                calc1 = 0;
+            }
+            else if (minusButtonClick == true)
+            {
+                calc2 = calc1 - double.Parse(calcBox.Text);
+                calcBox.Text = calc2.ToString();
+                calc1 = 0;
+            }
+            else if (multiplyButtonClick == true)
+            {
+                calc2 = calc1 * double.Parse(calcBox.Text);
+                calcBox.Text = calc2.ToString();
+                calc1 = 0;
+            }
+            else if (divideButtonClick == true)
+            {
+                calc2 = calc1 / double.Parse(calcBox.Text);
+                calcBox.Text = calc2.ToString();
+                calc1 = 0;
+            }
+        
         }
 
+        // . decimal point button
         private void ButtonDecimal_Click(object sender, RoutedEventArgs e)
         {
-            
+            calcBox.Text += ".";
         }
-
+        // clear button
         private void ButtonClear_Click(object sender, RoutedEventArgs e)
         {
-            
+
+            calcBox.Text = "";
         }
 
 
     }
 }
+
